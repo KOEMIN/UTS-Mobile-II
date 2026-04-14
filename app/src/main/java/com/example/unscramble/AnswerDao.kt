ce008b830996806afbe55a603640953c4702bfa9
KOEMIN <raufahafid87@gmail.com>
2026-04-14 19:46:40 +0700

    memperbaiki entity dan Dao

--- app/src/main/java/com/example/unscramble/AnswerDao.kt
+++ app/src/main/java/com/example/unscramble/AnswerDao.kt
@@ -4,11 +4,17 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.unscramble.data.UserAnswer
+import kotlinx.coroutines.flow.Flow
 
 
 interface AnswerDao {
     @Insert
+
     suspend fun insertWord(word: UserAnswer)
+
     @Query("SELECT * FROM correct_answers ORDER by id DESC")
-    fun getALLanswers(): List<UserAnswer>
+    fun getALLanswers(): Flow<List<UserAnswer>>
+
+    @Query("DELETE FROM correct_answers")
+    suspend fun deleteAll()
 }
